bfccd0e17ede2ebe0d0bbdac410d7d73c4ae39e8
Wayer52 <juanjsaavedra52@gmail.com>
2023-07-10 14:30:42 -0500

    Continué hasta la clase 22

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const routerApi = require('./routes');
 
+const { logErrors, errorHandler, boomErrorHandler } = require('./middlewares/errorHandler')
 
 const app = express();
 const port = 3000;
@@ -16,6 +17,10 @@ app.get('/nueva-ruta', (req, res) => {
 })
 
 routerApi(app);
+
+app.use(logErrors);
+app.use(boomErrorHandler);
+app.use(errorHandler);
 
 app.listen(port, () => {
   console.log(`La aplicación se esta escuchando en http://localhost:${port}`);
--- routes/categoriesRouter.js
+++ routes/categoriesRouter.js
@@ -1,12 +1,20 @@
 const express = require('express');
 
+const CategoriesService = require('../services/categoriesService');
+
 const router = express.Router();
+const service = new CategoriesService;
+
+router.get('/', (req, res) => {
+  const categories = service.find();
+  res.json(categories);
+})
 
 router.get('/categories/:categoryId/products/:productId', (req, res) => {
-  const { categoryId, productId } = req.params;
+  const { categoryId, productId } = service.params;
+  const categoryAndProduct = service.findOne(categoryId, productId)
   res.json({
-    categoryId,
-    productId
+    categoryAndProduct
   });
 })
 
