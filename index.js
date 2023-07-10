32b16444991bf2df91e549aaba636ff5dd2a2ff8
Wayer52 <juanjsaavedra52@gmail.com>
2023-07-10 16:17:42 -0500

    Use el cors y defini desde donde se puede conectar

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const routerApi = require('./routes');
+const cors = require('cors');
 
 const { logErrors, errorHandler, boomErrorHandler } = require('./middlewares/errorHandler')
 
@@ -7,6 +8,19 @@ const app = express();
 const port = 3000;
 
 app.use(express.json());
+
+const whitelist = ['http://192.168.10.17:5500', 'https://myapp.co'];
+const options = {
+  origin: (origin, callback) => {
+    if (whitelist.includes(origin)) {
+      callback(null, true);
+    } else {
+      callback(new Error('No permitido'));
+    }
+  }
+}
+
+app.use(cors(options));
 
 app.get('/', (req, res) => {
   res.send('Hola mi server en express');
